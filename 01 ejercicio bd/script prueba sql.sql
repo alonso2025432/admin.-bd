7894bfe2f1c6c0db98d8c92cae03043038d54f3d
alonso2025432 <alonsozuniga@liceovvh.cl>
2025-03-10 12:40:00 -0300

    ok

---
+++ 01 ejercicio bd/script prueba sql.sql
@@ -0,0 +1,9 @@
+create database prueba;
+
+ git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
+
+use prueba;
+
+create table usuario(id INT, username VARCHAR(50),
+password VARCHAR(50));
